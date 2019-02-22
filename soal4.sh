c1d8d0a7a19cbec7ef1bd8a4cb80aac7f509db26
TRIA NUR AISYAH AMINI <35224493+mochimoe@users.noreply.github.com>
2019-02-22 10:08:50 +0700

    Update soal4.sh

--- soal4.sh
+++ soal4.sh
@@ -6,4 +6,5 @@ ups=ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ
 out=$(date +"%H:%M %d-%m-%y")
 shif=$(date +"%k")
 
+`sudo mkdir -p ~/Documents/Sisop1`
 cat /var/log/syslog | tr "${letter:0:26}" "${letter:${shif}:26}" |tr "${ups:0:26}" "${ups:${shif}:26}"  > ~/Documents/Sisop1/"$out"

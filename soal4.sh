bb33e61cdeaf6f5358c23160d4d1827718a05e09
Iotatfan <42773954+Iotatfan@users.noreply.github.com>
2019-02-22 14:44:18 +0700

    Minor Typo

--- soal4.sh
+++ soal4.sh
@@ -1,10 +1,10 @@
 
 
-letter=abcdefghojklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz
+letter=abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz
 ups=ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ
 
 out=$(date +"%H:%M %d-%m-%y")
 shif=$(date +"%k")
 
-`sudo mkdir -p ~/Documents/Sisop1`
-cat /var/log/syslog | tr "${letter:0:26}" "${letter:${shif}:26}" |tr "${ups:0:26}" "${ups:${shif}:26}"  > ~/Documents/Sisop1/"$out"
+`sudo mkdir -p ~/Documents/SyslogBackUp`
+cat /var/log/syslog | tr "${letter:0:26}" "${letter:${shif}:26}" |tr "${ups:0:26}" "${ups:${shif}:26}"  > ~/Documents/SyslogBackUp/"$out"
